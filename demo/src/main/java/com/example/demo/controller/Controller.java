12184c1c3f2ac7281a0cabbd1b8758a2b17ffdda
vikram kuamr reddy <vikram kuamr reddy@LAPTOP-IQHS2IH5>
2020-01-31 21:12:01 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.TicketDao;
+import com.example.demo.model.Ticket;
+
+@RestController
+@RequestMapping("/ticket")
+public class Controller {
+	
+	@Autowired
+	private TicketDao ticketDao;
+	
+	
+	@PostMapping("/booktickets")
+	public List<Ticket>  bookTickets(@RequestBody List<Ticket> tickets)
+	{
+		return (List<Ticket>) ticketDao.saveAll(tickets);
+		
+		
+	}
+	
+	@GetMapping("/gettickets")
+	public List<Ticket> getTickets(@RequestBody List<Ticket> tickets)
+	{
+		
+		return (List<Ticket>) ticketDao.findAll();
+	
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ demo/src/main/java/com/example/demo/model/Ticket.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Ticket")
+public class Ticket {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private double amount;
+	private String category;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	@Override
+	public String toString() {
+		return "Ticket [id=" + id + ", amount=" + amount + ", category=" + category + "]";
+	}
+	
+
+}
